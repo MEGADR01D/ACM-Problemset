8569f6e7269c4f3e81d7a30fe9072e173f115e4a
MEGADR01D <megadroid5678@gmail.com>
2019-04-23 12:45:54 +0200

    Create Sales.java

---
+++ uVa/1260 - Sales/Sales.java
@@ -0,0 +1,28 @@
+/*
+* 1260 - Sales: https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3701
+*/
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        PrintWriter writer = new PrintWriter(System.out);
+        int t = reader.nextInt(), n, sum;
+        int[] a;
+
+        for (int i = 0; i < t; i++) {
+            sum = 0;
+            n = reader.nextInt();
+            a = new int[n];
+            for (int j = 0; j < n; j++) a[j] = reader.nextInt();
+            for (int j = 1; j < n; j++) for (int k = 0; k < j; k++) sum += a[j] >= a[k] ? 1 : 0;
+            writer.println(sum);
+        }
+        reader.close();
+        writer.close();
+    }
+}
