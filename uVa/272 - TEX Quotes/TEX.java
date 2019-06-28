4fe6c4cb36ba656e355c06e930b9139cc81837a2
MEGADR01D <megadroid5678@gmail.com>
2019-06-28 16:19:41 +0200

    Create TEX.java

---
+++ uVa/272 - TEX Quotes/TEX.java
@@ -0,0 +1,33 @@
+/*
+* 272 - TEX Quotes: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=4&page=show_problem&problem=208
+*/
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        PrintWriter writer = new PrintWriter(System.out);
+        String str;
+        boolean open = true;
+
+        while (reader.hasNextLine()) {
+            str = reader.nextLine();
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '"') if (open) {
+                    str = str.replaceFirst("\"", "``");
+                    open = false;
+                } else {
+                    str = str.replaceFirst("\"", "''");
+                    open = true;
+                }
+            }
+            writer.println(str);
+        }
+        reader.close();
+        writer.close();
+    }
+}
